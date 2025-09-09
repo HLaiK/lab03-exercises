88fdb31077c887955748521caf999da78f86fa36
Laiza Ktul <hrktul@gmail.com>
2025-09-09 17:29:32 -0400

    Completed more practice

--- python/find_duplicates.py
+++ python/find_duplicates.py
@@ -22,3 +22,29 @@ if __name__ == "__main__":
     print("Sample 2:", find_duplicates_nested_loop(sample2))
     print("Sample 3:", find_duplicates_nested_loop(sample3))
     print("Sample 4:", find_duplicates_nested_loop(sample4))
+
+print("\nSolving with a Dictionary/Map")
+
+def find_duplicates_dictionary_map(l: list) -> list:
+    counts = {}
+    duplicates = []
+
+    for num in l:
+        counts[num] = counts.get(num, 0) + 1
+
+    for num, count in counts.items():
+        if count > 1:
+            duplicates.append(num)
+
+    return duplicates
+
+if __name__ == "__main__":
+    sample1 = [3, 7, 5, 6, 7, 4, 8, 5, 7, 66]
+    sample2 = [3, 5, 6, 4, 4, 5, 66, 6, 7, 6]
+    sample3 = [3, 0, 5, 1, 0]
+    sample4 = [3]
+    
+    print("Sample 1:", find_duplicates_dictionary_map(sample1))
+    print("Sample 2:", find_duplicates_dictionary_map(sample2))
+    print("Sample 3:", find_duplicates_dictionary_map(sample3))
+    print("Sample 4:", find_duplicates_dictionary_map(sample4))
